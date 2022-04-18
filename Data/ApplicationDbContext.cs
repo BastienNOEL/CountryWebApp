3f8a9d4efefc4f9da6a25ff65d5661c307939f21
BastienNOEL <bastien.noel@hotmail.fr>
2022-04-18 12:49:10 +0200

    mon premier commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CountryWebApp.Models;
+
+namespace CountryWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CountryWebApp.Models.Country> Country { get; set; }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,15 @@
+﻿namespace CountryWebApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        public string CountryInfo { get; set; }
+
+        public Country()
+        {
+
+        }
+
+    }
+}
